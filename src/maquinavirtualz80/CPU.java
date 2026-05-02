1c915853d3b0cee2c79250f7c595e20700ea5c8f
Milena Ferreira <maferreira@inf.ufpel.edu.br>
2026-05-02 00:29:16 -0300

    Implementação das funções demais funções (add, and, xor, or, cp, jp, jr call, ret, etc)

--- src/maquinavirtualz80/CPU.java
+++ src/maquinavirtualz80/CPU.java
@@ -27,6 +27,8 @@ public class CPU {
         memory = new Memory();
         registers = new Registers();
         halted = false;
+        
+        registers.SP = 0xFFFE;
     }
     //CRIA O RUN
     public void run() {
@@ -58,7 +60,8 @@ public class CPU {
 
         case 0x00: // NOP
             break;
-        
+            
+        //====================================================          
         case 0x3E: // LD A, n
             int value = memory.read(registers.PC); // pega o próximo byte
             registers.PC++; // avança o PC
@@ -68,6 +71,7 @@ public class CPU {
             registers.flags.setSign((registers.A & 0X80) != 0);
             break;
             
+        //====================================================              
         case 0x3C: // INC A
             registers.A = (registers.A + 1) & 0xFF;
             // FLAGS
@@ -75,22 +79,181 @@ public class CPU {
             registers.flags.setSign((registers.A & 0x80) != 0);
             break;
             
+         //====================================================             
         case 0x76: // HALT
             halted = true;
             break;
-        
+            
+        //====================================================          
         case 0x3D: //DEC A
             registers.A = (registers.A - 1) & 0xFF;
             //FLAGS
             registers.flags.setZero(registers.A == 0);
             registers.flags.setSign((registers.A & 0X80) != 0);
             break;
+         
+        //====================================================  
+        case 0xC6: // ADD A, n
+            int valueAdd = memory.read(registers.PC);
+            registers.PC++;
+            
+            int result = registers.A + valueAdd;
+            
+            //Carry (passou de 255?)
+            registers.flags.setCarry(result > 0xFF);
+            
+            //Mantém só 8 bits
+            registers.A = result & 0xFF;
+            
+            //FLAGS
+            registers.flags.setZero(registers.A == 0);
+            registers.flags.setSign((registers.A & 0x80) != 0);
+            
+            break;
 
+        //====================================================  
+        case 0xD6: // SUB A, n
+            int valueSub = memory.read(registers.PC);
+            registers.PC++;
+            
+            int resultSub = registers.A - valueSub;
+            
+            //Carry (em subtração = borrow)
+            registers.flags.setCarry(resultSub < 0);
+            registers.A = resultSub & 0xFF;
+            
+            //FLAGS
+            registers.flags.setZero(registers.A == 0);
+            registers.flags.setSign((registers.A & 0x80) != 0);
+            
+            break;
+            
+        //====================================================  
+        case 0xE6: //AND A, n
+            int valueAnd = memory.read(registers.PC);
+            registers.PC++;
+            
+            registers.A = registers.A & valueAnd;
+            
+            //FLAGS
+            registers.flags.setZero(registers.A == 0);
+            registers.flags.setSign((registers.A & 0x80) != 0);
+            registers.flags.setCarry(false); //AND zera carry
+            
+            break;
+            
+        //==================================================== 
+        case 0xF6: //OR A, n
+            int valueOr = memory.read(registers.PC);
+            registers.PC++;
+            
+            registers.A = registers.A | valueOr;
+            
+            //FLAGS
+            registers.flags.setZero(registers.A == 0);
+            registers.flags.setSign((registers.A & 0x80) != 0);
+            registers.flags.setCarry(false); 
+            
+            break;
+            
+        //==================================================== 
+        case 0xEE: // XOR A, n
+            int valueXor = memory.read(registers.PC);
+            registers.PC++;
+            
+            registers.A = registers.A ^ valueXor;
+            
+            //FLAGS
+            registers.flags.setZero(registers.A == 0);
+            registers.flags.setSign((registers.A & 0x80) != 0);
+            registers.flags.setCarry(false); 
+            
+            break;
+           
+        //==================================================== 
+        case 0xFE: // CP A, n
+            int valueCp = memory.read(registers.PC);
+            registers.PC++;
+            
+            int resultCp = registers.A - valueCp;
+            
+            //FLAGS
+            registers.flags.setZero((resultCp & 0xFF) == 0);
+            registers.flags.setSign((resultCp & 0x80) != 0);
+            registers.flags.setCarry(resultCp < 0); 
+            
+            break;
+            
+        //==================================================== 
+        case 0xC3: //JP nn
+            int low = memory.read(registers.PC);
+            registers.PC++;
+            
+            int high = memory.read(registers.PC);
+            registers.PC++;
+            
+            int adress = (high << 8) | low;
+            
+            registers.PC = adress;
+            
+            break;
+            
+        //==================================================== 
+        case 0x18: //JR e
+            int offset = memory.read(registers.PC);
+            registers.PC++;
+            
+            //converte pra signed (byte com sinal)
+            if (offset > 127) {
+                offset = offset - 256;
+            }
+            
+            registers.PC = registers.PC + offset;
+            
+            break;
+            
+        //==================================================== 
+        case 0xCD: //CALL nn
+            int lowCall = memory.read(registers.PC);
+            registers.PC++;
+            
+            int highCall = memory.read(registers.PC);
+            registers.PC++;
+            
+            int adressCall = (highCall << 8) | lowCall;
+            
+            //empilha o pc atual
+            registers.SP--;
+            memory.write(registers.SP, (registers.PC >> 8) & 0xFF); //HIGH
+            
+            registers.SP--;
+            memory.write(registers.SP, registers.PC & 0xFF); //low
+            
+            //salta
+            registers.PC = adressCall;
+            
+            break;
+            
+        //==================================================== 
+        case 0xC9: // RET
+        int lowRet = memory.read(registers.SP);
+        registers.SP++;
+
+        int highRet = memory.read(registers.SP);
+        registers.SP++;
+
+        int returnAddress = (highRet << 8) | lowRet;
+
+        registers.PC = returnAddress;
+
+        break;
+        
+        //====================================================
         default:
             System.out.println("Opcode não implementado: " + Integer.toHexString(opcode));
             halted = true;
             break;
     }
-}
+    }
     
 }
--- src/maquinavirtualz80/Main.java
+++ src/maquinavirtualz80/Main.java
@@ -118,7 +118,7 @@ public class Main {
         // =========================
         // TESTE 8 — DEC A (1 → 0)
         // =========================
-CPU cpu4 = new CPU();
+        CPU cpu4 = new CPU();
 
         cpu4.getMemory().write(0, 0x3E); // LD A, n
         cpu4.getMemory().write(1, 0x01); // A = 1
@@ -132,6 +132,203 @@ CPU cpu4 = new CPU();
         System.out.println("Zero = " + cpu4.getRegisters().flags.getZero());
         System.out.println("Sign = " + cpu4.getRegisters().flags.getSign());
         
+        // ============================
+        // TESTE 9 - ADD
+        // ============================
+        CPU cpu5 = new CPU();
+        
+        cpu5.getMemory().write(0, 0x3E); //LD A , n
+        cpu5.getMemory().write(1, 0x05); //ADD = 5
+        
+        cpu5.getMemory().write(2, 0xC6); //ADD A , n
+        cpu5.getMemory().write(3, 0x03); // +3
+        
+        cpu5.getMemory().write(4, 0x76); //HALT
+        
+        cpu5.run();
+        
+        System.out.println("\nTESTE 9 - ADD:");
+        System.out.println("A = " + cpu5.getRegisters().A);
+        System.out.println("Zero = " + cpu5.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu5.getRegisters().flags.getSign());
+        System.out.println("Carry = " + cpu5.getRegisters().flags.getCarry());
+        
+        // ============================
+        // TESTE 10 - SUB
+        // ============================
+        CPU cpu6 = new CPU();
+
+        cpu6.getMemory().write(0, 0x3E); // LD A, n
+        cpu6.getMemory().write(1, 10);   // A = 10
+
+        cpu6.getMemory().write(2, 0xD6); // SUB A, n
+        cpu6.getMemory().write(3, 3);    // -3
+
+        cpu6.getMemory().write(4, 0x76); // HALT
+
+        cpu6.run();
+
+        System.out.println("\nTESTE 10 — SUB:");
+        System.out.println("A = " + cpu6.getRegisters().A);
+        System.out.println("Zero = " + cpu6.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu6.getRegisters().flags.getSign());
+        System.out.println("Carry = " + cpu6.getRegisters().flags.getCarry());
+        
+        // ============================
+        // TESTE 11 - AND
+        // ============================
+        CPU cpu7 = new CPU();
+
+        cpu7.getMemory().write(0, 0x3E);
+        cpu7.getMemory().write(1, 0b10101010); // 170
+
+        cpu7.getMemory().write(2, 0xE6);
+        cpu7.getMemory().write(3, 0b11110000); // 240
+
+        cpu7.getMemory().write(4, 0x76);
+
+        cpu7.run();
+
+        System.out.println("\nTESTE 11 — AND:");
+        System.out.println("A = " + cpu7.getRegisters().A);
+        System.out.println("Zero = " + cpu7.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu7.getRegisters().flags.getSign());
+        System.out.println("Carry = " + cpu7.getRegisters().flags.getCarry());
+        
+        // ============================
+        // TESTE 12 - OR
+        // ============================
+        CPU cpu8 = new CPU();
+
+        cpu8.getMemory().write(0, 0x3E); //LD A, n
+        cpu8.getMemory().write(1, 0x0A); // 1010
+
+        cpu8.getMemory().write(2, 0xF6); //OR n
+        cpu8.getMemory().write(3, 0X05); // 0101 ou 5
+
+        cpu8.getMemory().write(4, 0x76); //halt
+
+        cpu8.run();
+
+        System.out.println("\nTESTE 12 — OR:");
+        System.out.println("A = " + cpu8.getRegisters().A);
+        System.out.println("Zero = " + cpu8.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu8.getRegisters().flags.getSign());
+        
+        // ============================
+        // TESTE 13 - XOR
+        // ============================
+        CPU cpu9 = new CPU();
+
+        cpu9.getMemory().write(0, 0x3E); //LD A, n
+        cpu9.getMemory().write(1, 0x0A); // 1010
+
+        cpu9.getMemory().write(2, 0xEE); //XOR n
+        cpu9.getMemory().write(3, 0x0C); // 12 (1100)
+
+        cpu9.getMemory().write(4, 0x76); //halt
+
+        cpu9.run();
+
+        System.out.println("\nTESTE 13 — XOR:");
+        System.out.println("A = " + cpu9.getRegisters().A);
+        System.out.println("Zero = " + cpu9.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu9.getRegisters().flags.getSign());  
+        
+        // ============================
+        // TESTE 14 - CP (igual)
+        // ============================
+        CPU cpu10 = new CPU();
+
+        cpu10.getMemory().write(0, 0x3E); //LD A, n
+        cpu10.getMemory().write(1, 0x0A); // A = 1010 (10)
+
+        cpu10.getMemory().write(2, 0xFE); //CP n
+        cpu10.getMemory().write(3, 0x0A); // compara com A (que é 10)
+
+        cpu10.getMemory().write(4, 0x76); //halt
+
+        cpu10.run();
+
+        System.out.println("\nTESTE 14 — CP:");
+        System.out.println("A = " + cpu10.getRegisters().A);
+        System.out.println("Zero = " + cpu10.getRegisters().flags.getZero());
+        System.out.println("Sign = " + cpu10.getRegisters().flags.getSign()); 
+        System.out.println("Carry = " + cpu10.getRegisters().flags.getCarry());        
+        
+        // ============================
+        // TESTE 15 - JP
+        // ============================
+        CPU cpu11 = new CPU();
+        
+        //PULA DIRETO PRA POSIÇÃO 5
+        cpu11.getMemory().write(0, 0xC3); //JP
+        cpu11.getMemory().write(1, 0x05); // low
+        cpu11.getMemory().write(2, 0x00); // high
+        
+        // Isso aqui deve ser IGNORADO
+        cpu11.getMemory().write(3, 0x3E); // LD A
+        cpu11.getMemory().write(4, 0xFF); // 255
+
+        // Destino do salto
+        cpu11.getMemory().write(5, 0x3E); // LD A
+        cpu11.getMemory().write(6, 0x0A); // A = 10
+
+        cpu11.getMemory().write(7, 0x76); // HALT
+
+        cpu11.run();
+
+        System.out.println("\nTESTE 15 — JP:");
+        System.out.println("A = " + cpu11.getRegisters().A);
+        
+        // ============================
+        // TESTE 16 - JR (forward)
+        // ============================
+        CPU cpu12 = new CPU();
+        
+        //JR +2
+        cpu12.getMemory().write(0, 0x18); //JR
+        cpu12.getMemory().write(1, 0x02); // +2
+        
+        // Isso aqui deve ser IGNORADO (pulado)
+        cpu12.getMemory().write(2, 0x3E); // LD A
+        cpu12.getMemory().write(3, 0xFF); // 255
+
+        // Destino do salto
+        cpu12.getMemory().write(4, 0x3E); // LD A
+        cpu12.getMemory().write(5, 0x0A); // A = 10
+
+        cpu12.getMemory().write(6, 0x76); // HALT
+
+        cpu12.run();
+
+        System.out.println("\nTESTE 16 — JR forward:");
+        System.out.println("A = " + cpu12.getRegisters().A);
+        
+        // ============================
+        // TESTE 17 - CALL + RET
+        // ============================    
+        CPU cpu13 = new CPU();
+        
+        //CALL 0006
+        cpu13.getMemory().write(0, 0xCD);
+        cpu13.getMemory().write(1, 0x06);
+        cpu13.getMemory().write(2,0x00);
+        
+        //CÓDIGO PRINCIPAL (EXECUTA DEPOIS DO RET)
+        cpu13.getMemory().write(3, 0x3E); // LD A
+        cpu13.getMemory().write(4, 0x01); // A = 1
+        cpu13.getMemory().write(5, 0x76); // HALT
+
+        // "função" (endereço 6)
+        cpu13.getMemory().write(6, 0x3E); // LD A
+        cpu13.getMemory().write(7, 0x0A); // A = 10
+        cpu13.getMemory().write(8, 0xC9); // RET
+
+        cpu13.run();
+
+        System.out.println("\nTESTE 17 — CALL + RET:");
+        System.out.println("A = " + cpu13.getRegisters().A);
         
     }
 }
